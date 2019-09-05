da002854d002d821e872e25ecd2fa7dd14612d14
Omegahub <Omegahub2019@gmail.com>
2019-09-04 22:38:45 -0300

    exercicio9

--- dist/exercicio10.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var exercicio9_1 = require("./exercicio9");
-var cuboNum = 2;
-console.log("o cubo \u00E9 : " + exercicio9_1.cubo(cuboNum));
--- exercicio10.ts
+++
@@ -1,4 +0,0 @@
-import { cubo } from "./exercicio9";
-
-let cuboNum:number = 2;
-console.log(`o cubo é : ${cubo(cuboNum)}`)
