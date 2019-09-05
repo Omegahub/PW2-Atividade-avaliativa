4efa3e35d3791d7bc535421c2674a2b499aacf02
Omegahub <Omegahub2019@gmail.com>
2019-09-04 22:37:39 -0300

    exercicio10

---
+++ dist/exercicio9.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+//func exercicio 4
+exports.parImpar = function (numero) {
+    if (numero % 2 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+//func exercicio 5
+exports.calcArea = function (comprimento, largura) {
+    return (comprimento * largura);
+};
+//func exercicio 6
+exports.cubo = function (cuboNum) { return (cuboNum * cuboNum * cuboNum); };
---
+++ exercicio9.ts
@@ -0,0 +1,26 @@
+
+//func exercicio 4
+export let parImpar = function(numero:number):boolean
+{
+    if(numero % 2 == 0)
+    {
+        return true
+    }
+    else
+    {
+       return false
+    }
+}
+
+
+
+//func exercicio 5
+export let calcArea = function(comprimento:number, largura:number):number
+{
+    return(comprimento * largura)
+}
+
+//func exercicio 6
+
+
+export let cubo = (cuboNum:number):number => (cuboNum * cuboNum * cuboNum)
