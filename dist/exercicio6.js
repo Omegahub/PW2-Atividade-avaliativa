5a4f96e161e4c1e559401f0e9016dc4e4a1e24f9
Omegahub <Omegahub2019@gmail.com>
2019-09-04 21:36:00 -0300

    exercicio6

---
+++ dist/exercicio6.js
@@ -0,0 +1,3 @@
+var cuboNum = 2;
+var cubo = function (cuboNum) { return (cuboNum * cuboNum * cuboNum); };
+console.log("o cubo \u00E9  : " + cubo(cuboNum));
---
+++ exercicio6.ts
@@ -0,0 +1,5 @@
+let cuboNum:number = 2;
+
+let cubo = (cuboNum:number):number => (cuboNum * cuboNum * cuboNum)
+
+console.log(`o cubo é  : ${cubo(cuboNum)}`)
