cd8f937607fd5eee01295b1be758e1291474a2a3
Omegahub <Omegahub2019@gmail.com>
2019-09-04 21:30:04 -0300

    exercicio5

---
+++ dist/exercicio5.js
@@ -0,0 +1,6 @@
+var largura = 20;
+var comprimento = 20;
+var calcArea = function (comprimento, largura) {
+    return (comprimento * largura);
+};
+console.log("area : " + calcArea(comprimento, largura));
---
+++ exercicio5.ts
@@ -0,0 +1,10 @@
+
+let largura:number =20;
+let comprimento:number =20;
+
+let calcArea = function(comprimento:number, largura:number):number
+{
+    return(comprimento * largura)
+}
+
+console.log(`area : ${calcArea(comprimento,largura)}`)
